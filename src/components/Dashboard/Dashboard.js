c8d4536b006d4a12f96ade20098b716b2fc84b21
David B <d.jbradley.ii@gmail.com>
2020-01-16 20:05:34 -0800

    Adds user info for directions:

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,6 +8,12 @@ export default class Dashboard extends React.Component{
     render(){
         return(
             <section className="dashboard">
+                <p className="user-info">Teams show up based on your user stats.</p>
+                <ul className="user-info">
+                    <li>Trades - Trade Confirmed: Removes items from your inventory. Trade Accepted adds the trade to your inventory.</li>
+                    <li>Available Teams - A list of gamers will be listed based on your current user stats.</li>
+                    <li>You can search your inventory below.</li>
+                </ul>
                 <UserTrades />
                 <UserTeamRequests />
                 <UserInventory />
